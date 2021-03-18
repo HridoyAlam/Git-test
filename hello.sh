9cadd1698ca8f4acea19f218f586e0d800ee951c
HridoyAlam <optimus5289@gmail.com>
2021-03-18 19:17:53 +0600

    first commit

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+#this is a comment
+echo "Hello world" # this is also comments
+
+echo our shell name is $BASH #give the name of the shells
+echo our shell version is$BASH_VERSION
+echo our home directory is $HOME
+echo our presents working directory is $PWD
+
+name=Hridoy
+echo name is $name
